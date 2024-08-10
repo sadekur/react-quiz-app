debd4576141894c5c5369c63ca7f2759f4e5f57d
Sadekur Rahman <shadekur.rahman60@gmail.com>
2024-08-10 21:58:04 +0600

    update login logout outh

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const app = initializeApp({
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_ID,
-  databaseURL: REACT_APP_DATABASE_URL,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
 });
 
 export default app;
