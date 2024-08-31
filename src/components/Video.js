e074aaf52ea2d8c260d15b3f326a3c0b687f71ed
Sadekur Rahman <shadekur.rahman60@gmail.com>
2024-08-31 18:23:46 +0600

    update firebase data adding dynamically but has problem

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,13 +1,13 @@
 import classes from "../styles/Video.module.css";
 import imagee from "../assets/images/3.jpg";
-export default function Video() {
+export default function Video({title, id, noq}) {
   return (
       <div className={classes.video}>
-        <img src={imagee} alt="Video Image" />
-        <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+        <img src={imagee} alt={title} />
+        <p>{title}</p>
         <div className={classes.qmeta}>
-          <p>10 Questions</p>
-          <p>Score : Not taken yet</p>
+          <p>{noq}</p>
+          <p>Total Post : {noq * 5}</p>
         </div>
       </div>
   );
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,31 +1,21 @@
 import { Link } from "react-router-dom";
 import classes from "../styles/Videos.module.css";
 import Video from "./Video";
+import useVideoList from "../hooks/useVideoList";
 
 export default function Videos() {
+   const{ loading, error, videos } = useVideoList();
   return (
     <div className={classes.videos}>
-      <Link to="/quiz">
-        <Video />
+    {videos.length > 0 &&
+    videos.map((video) => (
+      <Link to="/quiz" key={video.youtubeID}>
+        <Video title={video.title} id={video.youtubeID} noq={video.noq} />
       </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
-      <Link to="/quiz">
-        <Video />
-      </Link>
+    ))}
+    {!loading && videos.length === 0 && <div>No data found!</div>}
+    {error && <div>There was an error!</div>}
+    {loading && <div>Loading...</div>}
     </div>
   );
 }
